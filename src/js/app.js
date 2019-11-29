b87f289b55c035cdce26b71b88eae0f799cf03d3
Dmytro Borshch <borsch.dima@gmail.com>
2019-11-29 19:35:26 +0300

    Almost done)

---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    new WOW().init();
+
+    const taskName = document.querySelector("#taskName");
+    const tasDescription = document.querySelector("#taskDescription");
+});
